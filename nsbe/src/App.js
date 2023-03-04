51140dde477943d53b8555ae0331e63b514bea4e
jamesaliev <james.aliev@gmail.com>
2023-03-04 16:09:00 -0500

    mergeAGAIN

--- nsbe/src/App.js
+++ nsbe/src/App.js
@@ -1,3 +1,26 @@
+import React from "react";
+import { Route, RouterProvider } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Home />}
+    >
+        <Route
+          path="/selection"
+          element={<Home />}
+        />
+    </Route>
+  )
+);
+
 function App() {
 
     return (
