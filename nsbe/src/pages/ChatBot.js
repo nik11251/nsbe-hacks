f41825f229806f686f1e8e96349cbd4b688285f3
Ritvik123487 <ritvik.jayanthi@gmail.com>
2023-03-04 15:30:10 -0500

    bnothing

---
+++ nsbe/src/pages/ChatBot.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+const ChatBot = () => {
+    
+
+    return (
+        <div>
+            <div style={{ textAlign: "center" }}>
+                <h1 style={{ fontSize: "48px", fontWeight: "bold", textDecoration: "underline" }}>
+                    ChatBot
+                </h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default ChatBot;
--- nsbe/src/pages/Home.js
+++ nsbe/src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React, {useState} from "react";
+import { useTransition } from "react-i18next";
 
-const About = () => {
+const Home = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const {t, i18n} = useTransition();
+
     //User inputs are required
 
     const handleUsernameChange = (event) => {
@@ -22,10 +24,18 @@ const About = () => {
 
     return (
         <div>
-            <h1>Login</h1>
-            <br></br>
-            <h1>Company Name</h1>
-       
+            <div style={{ textAlign: "center" }}>
+                <h1 style={{ fontSize: "48px", fontWeight: "bold", textDecoration: "underline" }}>
+                    {t("Login")}
+                </h1>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <img src="Utoronto_coa.svg.png" alt="left-image" style={{ height: "50px", marginRight: "10px" }} />
+                    <h2 style={{ fontSize: "32px", fontWeight: "bold" }}>Company Name</h2>
+                    <img src="NSBELogo_Color_withName" alt="right-image" style={{ height: "50px", marginLeft: "10px" }} />
+                </div>
+            </div>
+
+
             <div>
                 <form onSubmit={handleSubmit}>
                     <label>
@@ -44,4 +54,7 @@ const About = () => {
         </div>
     );
 }
-export default Home;
+export default Home; //With translation
+
+
+
