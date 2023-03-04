516daa7e5d3fc95f82a55f5609411081930bea1c
Nikifor Kiselev <nikifor.kiselev@gmail.com>
2023-03-04 15:27:49 -0500

    router

--- nsbe/src/App.js
+++ nsbe/src/App.js
@@ -1,19 +1,36 @@
+import React from "react";
+import { Route, RouterProvider } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Home />}
+    >
+        <Route
+          path="/selection"
+          element={<Home />}
+        />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div>
-      <header>
-        <a
 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React fuck react
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <NavBar/>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </div>
+    );
 }
 
 export default App;
