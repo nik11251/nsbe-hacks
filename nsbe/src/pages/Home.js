62673411d4f3bd17f8b514655ab21701b2949cc8
Ritvik123487 <ritvik.jayanthi@gmail.com>
2023-03-05 01:49:50 -0500

    this guy edits main

--- nsbe/src/pages/Home.js
+++ nsbe/src/pages/Home.js
@@ -17,18 +17,17 @@ const Home = () => {
         prompt: `Complete this sentence: "${input}"`,
         model: 'text-davinci-002',
         max_tokens: 100,
-        temperature:1.2,
+        temperature: 1,
         n: 1,
-        //stop: ".",
       },
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer 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`,
         },
       }
     );
-  
+
     return response.data.choices[0].text;
   };
 
@@ -40,7 +39,7 @@ const Home = () => {
       console.error(error);
     }
   }
-  
+
 
   useEffect(() => {
     const name = "This project has been developed for the hackathon NSBE. It is meant to assist minorities by easing their troubles my providing them access to well trusted therapists working on pro bono cases. Furthermore, the website utilizes an API from OpenAi to integrate a chat bot for users to ask general or inquisitive questions. We have also provided users with the ability to speak to like minded individuals about their problems via webcams and mic, while allowing them to rate their experience at the end of a call. These ratings will be used ti match those of similar levels together, hopefully bettering the experience for users. We pride ourselves in inclusivity and are always working towards supporting those in need.";
@@ -71,36 +70,60 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="header">
-        <h1 className="login">NSBE</h1>
-        <img src={require("./Uoft.png")} className="image" alt="Uoft logo" />
-      </div>
 
-      <div className="company-name">
-        <b>{companyName}</b>
-      </div>
+<div>
 
-      {showImages && (
-        <div className="image-container">
-        <img src={require("./people.jpg")} alt="people" />
-      </div>
-      )}
+<div class="grid-container">
+  <div class="grid-item">
+    {/* <!-- Left column content goes here --> */}
+    <div className="header">
 
-<div className="container">
-      <h2>Tell me something, and I'll tell you more</h2>
-      <textarea
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-        rows={5}
-        placeholder="Type in some words and I'll finish the rest..."
-      />
-      <button className="button" onClick={handleClick}>Complete Sentence</button>
-      {completedSentence && <p>Completed sentence: {completedSentence}</p>}
+           <h1 className="login">NSBE</h1>
+
+           <img src={require("./Uoft.png")} className="image" alt="Uoft logo" />
+         </div>
+
+         <div className="company-name">
+           <b>{companyName}</b>
+
+         </div>
+
+
+
+        {showImages && (
+           <div className="image-container">
+             <img src={require("./people.jpg")} alt="people" />
+           </div>
+         )}
+
+  </div>
+  <div class="grid-item">
+    <div class="top-row">
+      {/* <!-- Top row of right column content goes here --> */}
+      <h2>Chat Bot</h2>
+           <textarea
+            value={input}
+             onChange={(event) => setInput(event.target.value)}
+             rows={10} cols={40}
+             placeholder="Type Here..."
+
+           />
+           <button className="button" onClick={handleClick}>
+             Complete Sentence
+           </button>
     </div>
-
-    
+    <div class="bottom-row">
+      {/* <!-- Bottom row of right column content goes here --> */}
+      {completedSentence && (
+             <div className="completed-sentence">Completed sentence: {completedSentence}</div>
+       )}
     </div>
+  </div>
+</div>
+
+</div>
+
+
   );
 };
 
