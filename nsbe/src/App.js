c0db586c4e0c4c2911d46ef571435ff19face597
Nikifor Kiselev <nikifor.kiselev@gmail.com>
2023-03-05 10:01:14 -0500

    router

--- nsbe/src/App.js
+++ nsbe/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, RouterProvider, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import useToken from "./hooks/useToken";
 import Home from "./pages/Home";
@@ -20,58 +20,59 @@ import VideoApp from "./pages/Video/VideoApp";
 function App() {
     const { token, setToken } = useToken()
 
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route
-                path="/"
-
-            >
-                <Route
-                    path="/nsbe-hacks"
-                    element={<Home />}
-                />
-                <Route
-                    path="/nsbe-hacks/login"
-                    element={<Login setToken={setToken} />}
-                />
-                <Route
-                    path="/nsbe-hacks/signup"
-                    element={<Signup setToken={setToken} />}
-                />
-                <Route
-                    path="/nsbe-hacks/video2"
-                    element={<Video2 />}
-                />
-                    <Route
-                        path="/nsbe-hacks/video2/create"
-                        element={<CreatePage />}
-                    />
-                    <Route
-                        path="/nsbe-hacks/video2/join"
-                        element={<JoinPage />}
-                    />
-                    <Route
-                        path="/nsbe-hacks/video2/room"
-                        element={<RoomPage />}
-                    />
-                    <Route
-                        path="/nsbe-hacks/video3"
-                        element={<Video3 />}
-                    />
-                    <Route
-                        path="/nsbe-hacks/video"
-                        element={<VideoApp />}
-                    />
-                
-            </Route>
-        )
-    );
-
     return (
         <div>
             <NavBar />
             <React.StrictMode>
-                <RouterProvider router={router} />
+
+                    <BrowserRouter>
+
+                        <Routes>
+                            <Route
+                                path="/"
+
+                            >
+                                <Route
+                                    path="/nsbe-hacks"
+                                    element={<Home />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/login"
+                                    element={<Login setToken={setToken} />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/signup"
+                                    element={<Signup setToken={setToken} />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video2"
+                                    element={<Video2 />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video2/create"
+                                    element={<CreatePage />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video2/join"
+                                    element={<JoinPage />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video2/room"
+                                    element={<RoomPage />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video3"
+                                    element={<Video3 />}
+                                />
+                                <Route
+                                    path="/nsbe-hacks/video"
+                                    element={<VideoApp />}
+                                />
+
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+    
             </React.StrictMode>
         </div>
     );
