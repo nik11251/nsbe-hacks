902c9e8696b6583499f0b4c4e40c49b73844f1a5
Nikifor Kiselev <nikifor.kiselev@gmail.com>
2023-03-04 19:49:38 -0500

    adding users and database

--- nsbe/src/App.js
+++ nsbe/src/App.js
@@ -1,35 +1,48 @@
 import React from "react";
-import { Route, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import useToken from "./hooks/useToken";
 import Home from "./pages/Home";
 import {
   createRoutesFromElements,
   createBrowserRouter,
+  Route,
 } from "react-router-dom";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      element={<Home />}
-    >
-        <Route
-          path="/selection"
-          element={<Home />}
-        />
-    </Route>
-  )
-);
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
+    const [token, setToken] = useToken()
+
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route
+                path="/"
+                
+            >
+            <Route
+                path="/nsbe-hacks"
+                element={<Home />}
+            />
+            <Route
+                path="/nsbe-hacks/login"
+                element={<Login setToken={setToken}/>}
+            />
+            <Route
+                path="/nsbe-hacks/signup"
+                element={<Signup setToken={setToken}/>}
+            />
+            </Route>
+        )
+    );
 
     return (
-      <div>
-        <NavBar/>
-        <React.StrictMode>
-          <RouterProvider router={router} />
-        </React.StrictMode>
-      </div>
+        <div>
+            <NavBar/>
+            <React.StrictMode>
+                <RouterProvider router={router}/>
+            </React.StrictMode>
+        </div>
     );
 }
 
